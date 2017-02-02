4b05f4c8d993f35681ec1167ebde9a18aefddf56
neonsec <fahidroid@gmail.com>
2017-02-02 11:02:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/articleone',(req,res)=>{
+    res.send("Article One is requested");
+})
+app.get('/articletwo',(req,res)=>{
+    res.send("Article Two is requested");
+})
+app.get('/articlethree',(req,res)=>{
+    res.send("Article Three is requested");
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
