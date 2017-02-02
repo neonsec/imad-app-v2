cf57bfcd9f0e79b6b7b4dc45b602be591dc60b77
fahidroid@gmail.com <fahidroid@gmail.com>
2017-02-02 11:49:17 +0530

    few minor changes

--- server.js
+++ server.js
@@ -8,25 +8,22 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleone',(req,res)=>{
 
+
+app.get('/articleone',(req,res)=>{
   res.sendFile(path.join(__dirname,'ui','articleone.html'));
 })
+
 app.get('/articletwo',(req,res)=>{
   res.sendFile(path.join(__dirname,'ui','articletwo.html'));
 })
+
 app.get('/articlethree',(req,res)=>{
   res.sendFile(path.join(__dirname,'ui',"articlethree.html"));
 
-    res.send("Article One is requested");
 })
-app.get('/articletwo',(req,res)=>{
-    res.send("Article Two is requested");
-})
-app.get('/articlethree',(req,res)=>{
-    res.send("Article Three is requested");
 
-})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
