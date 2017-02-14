6e1134de593a805de259ff19d4694ae3139cb99b
abcd.xyz <fahidroid@gmail.com>
2017-02-14 19:57:54 +0530

    abc

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/articleone',(req,res)=>{
+app.get('/profile',(req,res)=>{
     res.sendFile(__dirname+'/ui/codepen/index.html');
 });
 app.get('/css/style.css',(req,res)=>{
@@ -26,13 +26,7 @@ app.get('/css/style.css',(req,res)=>{
 app.get('/js/index.js',(req,res)=>{
 	res.sendFile(__dirname+'/ui/codepen/js/index.js');
 })
-app.get('/articletwo',(req,res)=>{
-    res.sendFile(__dirname+'/ui/article-two.html');
-});
 
-app.get('/articlethree',(req,res)=>{
-    res.sendFile(__dirname+'/ui/article-three.html');
-});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
