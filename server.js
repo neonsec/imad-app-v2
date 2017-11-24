75839540fc2527c4c745b36db197e8d3c5356b32
Ubuntu <ubuntu@ip-172-31-27-166.us-east-2.compute.internal>
2017-11-24 05:34:00 +0000

    test

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get("/test", function(req,res) {
+	res.send("hi");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
